0591a929c8d90401cf13d77c373a93cc72009cf5
Edney Fillipi <e.fillipi@gmail.com>
2022-02-07 16:38:33 -0300

    :sparkles: docker-composer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { TweetsModule } from './modules/tweets/tweets.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TweetsModule],
+  imports: [
+    ConfigModule.forRoot(),
+
+    MongooseModule.forRoot(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+    }),
+    TweetsModule,
+  ],
 })
 export class AppModule {}
