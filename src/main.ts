ca87cb89de778441757bf52aac060049af95c3e8
Edney Fillipi <e.fillipi@gmail.com>
2022-02-10 06:50:28 -0300

    :sparkles: cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 const PORT = process.env.API_PORT;
 
 async function server() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(PORT);
 }
 server().then(() => {
